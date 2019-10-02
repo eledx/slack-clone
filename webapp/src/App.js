4fd23acf1685f4cb406274cdfd00a85faf72dc3b
Monia-L <monia.laoufi@gmail.com>
2019-10-02 21:17:58 +0200

    Clean the first code.i deleted component menu

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import Channel from './Components/Channel.js';
-import Menu from './Components/menu.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Menu />
       <Channel />
     </div>
   );
