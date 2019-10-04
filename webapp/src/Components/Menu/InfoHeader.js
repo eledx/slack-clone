63fb75d4c3ae99cf670000dd2543f95ed8cbddc3
Anthony <anthonyverret2436@gmail.com>
2019-10-04 10:18:18 +0200

    Create InfoHeader & SearchBar component.

---
+++ webapp/src/Components/Menu/InfoHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Menu.css'
+
+const InfoHeader = () => {
+  return (
+    <div>
+        
+    </div>
+  );
+};
+
+export default InfoHeader;
---
+++ webapp/src/Components/Menu/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Menu.css'
+
+const SearchBar = () => {
+  return (
+    <div>
+        
+    </div>
+  );
+};
+
+export default SearchBar;
