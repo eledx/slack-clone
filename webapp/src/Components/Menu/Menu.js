1a08e405b69914958971042d5fe096256c5b1947
Anthony <anthonyverret2436@gmail.com>
2019-10-04 09:56:53 +0200

    Fix drop down menu channels.

--- webapp/src/Components/Menu/Menu.js
+++ webapp/src/Components/Menu/Menu.js
@@ -5,34 +5,42 @@ import BurgerButton from './BurgerButton';
 class Menu extends React.Component {
   // On créer un état qui (ici) est un booléen false pour le moment car un état doit changer par la suite
   state = {
-    isOpen: false,
+    isOpenMenu: false,
+    isOpenDropDown: false,
   };
 
   // On créer une fonction fléchée qui aura pour objectif à son déclenchement de configuré l'état déclaré plus tôt
-  toggleIsOpen = () => {
+  toggleIsOpenMenu = () => {
     this.setState({
       // On configure l'état isOpen pour qu'il passe de false à true et de true à false. On fait de lui un toggle
-      isOpen: !this.state.isOpen,
+      isOpenMenu: !this.state.isOpenMenu,
+      isOpenDropDown: !this.state.isOpenDropDown,
+    });
+  };
+  toggleIsOpenDropDown = () => {
+    this.setState({
+      isOpenDropDown: !this.state.isOpenDropDown,
     });
   };
 
+
   render() {
-    //   On créer une constante burgerButton qui vérifie si isOpen est il est égal à true (on set la classe de la balise à 'burger_button open_burger' ) ou si égal à false (on set la classe de la balise à 'burger_button' )
-    const burgerButton = this.state.isOpen
+    //   On créer une constante burgerButton qui vérifie si isOpenMenu est il est égal à true (on set la classe de la balise à 'burger_button open_burger' ) ou si égal à false (on set la classe de la balise à 'burger_button' )
+    const burgerButton = this.state.isOpenMenu
       ? 'burger_button open_burger'
       : 'burger_button';
 
     return (
       <nav role="navigation" className="nav-bar">
-        {/* On appel le composant BurgerButton et on lui fais passer en paramètre la fonction toggleIsOpen et burgerButton */}
+        {/* On appel le composant BurgerButton et on lui fais passer en paramètre la fonction toggleIsOpenMenu et burgerButton */}
         <BurgerButton
-          fonction={this.toggleIsOpen}
+          fonction={this.toggleIsOpenMenu}
           burgerButton={burgerButton}
         />
-        {/* si isOpen est égal à true on set la classe à 'is-visible-in-mobile' */}
+        {/* si isOpenMenu est égal à true on set la classe à 'is-visible-in-mobile' */}
         <ul
           className={
-            this.state.isOpen
+            this.state.isOpenMenu
               ? 'left-navbar is-visible-in-mobile'
               : 'left-navbar'
           }
@@ -40,10 +48,10 @@ class Menu extends React.Component {
           <li>
             <a href="#">Accueil</a>
           </li>
-          <li onClick={this.toggleIsOpen}>
+          <li onClick={this.toggleIsOpenDropDown}>
             <a
               className={
-                this.state.isOpen
+                this.state.isOpenDropDown
                   ? 'list-element-channels-open'
                   : 'list-element-channels'
               }
@@ -53,7 +61,7 @@ class Menu extends React.Component {
             </a>
             <ul
               className={
-                this.state.isOpen ? 'channels-list-open' : 'channels-list'
+                this.state.isOpenDropDown ? 'channels-list-open' : 'channels-list'
               }
             >
               <li>
