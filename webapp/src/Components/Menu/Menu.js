12a78ec0b7f5dc1385308e5afc4ffceeb119b7da
Monia-L <monia.laoufi@gmail.com>
2019-11-13 09:57:47 +0100

    clean code and conform it withairb&n convention

--- webapp/src/Components/Menu/Menu.js
+++ webapp/src/Components/Menu/Menu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { InputGroup, InputGroupAddon, Input, Button } from 'reactstrap';
+import { InputGroup, InputGroupAddon, Button } from 'reactstrap';
 import BurgerButton from './BurgerButton';
 import InfoHeader from './InfoHeader';
 import SearchBar from './SearchBar';
